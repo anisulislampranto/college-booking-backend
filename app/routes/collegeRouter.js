57ed0d938664482d3f8924cc71bfe0abaa3457d1
Anisul Islam <anisulislam.pranto@gmail.com>
2024-10-03 12:58:07 +0600

    college create commented

--- app/routes/collegeRouter.js
+++ app/routes/collegeRouter.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const {
   getAllColleges,
-  createCollege,
+  // createCollege,
   getCollege,
-  updateCollege,
+  // updateCollege,
 } = require("../controllers/collegeController");
-const { isLoggedIn } = require("../controllers/authController");
-const upload = require("../config/multerConfig");
+// const { isLoggedIn } = require("../controllers/authController");
+// const upload = require("../config/multerConfig");
 
 const router = express.Router();
 
 router.get("/", getAllColleges);
 router.get("/:id", getCollege);
-router.post("/create", isLoggedIn, upload.single("image"), createCollege);
-router.patch("/update/:id", upload.single("image"), updateCollege);
+// router.post("/create", isLoggedIn, upload.single("image"), createCollege);
+// router.patch("/update/:id", upload.single("image"), updateCollege);
 
 module.exports = router;
