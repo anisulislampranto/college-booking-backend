040d78d72f110cd919d1ef03220a96ae5255a95d
Anisul Islam <anisulislam.pranto@gmail.com>
2024-10-05 21:51:46 +0600

    removed images && added multer image size restriction

--- app/config/multerconfig.js
+++ app/config/multerconfig.js
@@ -9,6 +9,9 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 1 * 1024 * 1024 },
+});
 
 module.exports = upload;
