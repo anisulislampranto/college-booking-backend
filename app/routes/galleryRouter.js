3bf3403ccc1f58cb5a96a13f94f8f381a005eec5
Anisul Islam <anisulislam.pranto@gmail.com>
2024-10-05 19:07:42 +0600

    multer issue check

--- app/routes/galleryRouter.js
+++ app/routes/galleryRouter.js
@@ -1,10 +1,10 @@
-// const upload = require("../config/multerConfig");
+const upload = require("../config/multerconfig");
 const { uploadImage, getImages } = require("../controllers/galleryController");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", getImages);
-// router.post("/create", upload.single("image"), uploadImage);
+router.post("/create", upload.single("image"), uploadImage);
 
 module.exports = router;
