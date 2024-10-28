c62021e345b39825a72592b26fb32c54f1fa2fba
Anisul Islam <anisulislam.pranto@gmail.com>
2024-10-29 00:32:58 +0600

    not working google login

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -18,25 +18,36 @@ exports.googleAuth = async (req, res, next) => {
     );
     const data = await userRes.json();
 
+    console.log("GData", data);
+
     const { email, name, picture } = data;
 
-    let user = await User.findOne({ email }).populate("colleges");
+    const user = await User.findOne({ email }).populate("colleges");
+
+    console.log("user", user);
 
     if (!user) {
-      user = await User.create({
-        name,
-        email,
-        image: picture,
+      console.log("createdUser");
+
+      const createdUser = await User.create({
+        name: "name",
+        email: "email@email.com",
+        image: "picture",
       });
+
+      console.log("createdUser", createdUser);
     }
 
     const token = createToken(user._id, user.email);
 
-    res.cookie("token", token).status(200).json({
-      message: "success",
-      token,
-      user,
-    });
+    res
+      .cookie("token", token)
+      .status(200)
+      .json({
+        message: "success",
+        token,
+        user: user ? user : createdUser,
+      });
   } catch (err) {
     res.status(500).json({
       message: "Internal Server Error",
